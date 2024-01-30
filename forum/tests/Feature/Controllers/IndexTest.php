9615906818a9b011bea586c9a18ccfe9a6083ed5
Ninoh van Dijke <47174@hoornbeeck.nl>
2024-01-30 15:55:46 +0100

    Les 9

--- forum/tests/Feature/Controllers/IndexTest.php
+++ forum/tests/Feature/Controllers/IndexTest.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Inertia\Testing\AssertableInertia;
 use function Pest\Laravel\get;
 
-it('Should return the correct comment', function () {
+it(' should return the correct component', function () {
     get(route('posts.index'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-        ->component('Posts/Index', true)
-        );
+        ->assertComponent('Posts/Index');
 });
 
 it('passes posts to the view', function () {
+
+    $posts = Post::factory(3)->create();
+
+    $posts->load('user');
+
     get(route('posts.index'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-            ->has('posts')
-        );
+        ->assertHasPaginatedResource('posts', PostResource::collection($posts->reverse()));
 });
