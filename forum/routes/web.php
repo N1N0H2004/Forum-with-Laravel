9a847d21935835a7f9107641c0a80035fa8b1a36
Ninoh van Dijke <47174@hoornbeeck.nl>
2024-02-01 10:33:00 +0100

    Les 15
    Ook welcome.vue aangepast en linkje naar posts toegevoegt

--- forum/routes/web.php
+++ forum/routes/web.php
@@ -17,6 +17,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
+        'canPosts' => Route::has('posts.index'),
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
