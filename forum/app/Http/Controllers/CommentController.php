440563963b24b3cd813077817a27cf4bf1ec88a3
Ninoh van Dijke <47174@hoornbeeck.nl>
2024-02-01 09:06:19 +0100

    Les 14

--- forum/app/Http/Controllers/CommentController.php
+++ forum/app/Http/Controllers/CommentController.php
@@ -9,15 +9,13 @@ class CommentController extends Controller
 {
     public function store(Request $request, Post $post) {
 
-        $data = $request->validate([
-           'body' => ['required', 'string', 'max:2500'],
-        ]);
+        $data = $request->validate(['body' => ['required', 'string', 'max:2500']]);
 
-        Comment::make($data)
-            ->user()->associate($request->user())
-            ->post()->associate($post)
-            ->save();
-
+        Comment::create([
+            ...$data,
+            'post_id' => $post->id,
+            'user_id' => $request->user()->id
+            ]);
 
         return to_route('posts.show', $post);
     }
